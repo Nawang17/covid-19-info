5e4894396c41e48b5b62a1dbfd614bf3d990cc56
Nawang17 <nawangofficial1010@gmail.com>
2022-01-20 10:44:59 -0500

    added overview page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
-import Navbar from "./components/Navbar";
-
+import moment from "moment";
 export default function Home({ posts }) {
   useEffect(() => {
     console.log(posts);
@@ -11,12 +9,44 @@ export default function Home({ posts }) {
   return (
     <>
       <Head>
-        <title>Overview</title>
-        <meta name="description" content="Total Cases " />
+        <title>Covid-19 Cases Overview</title>
+        <meta name="description" content="Overview of Covid-19 cases. " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <section className={styles.section}>
+          <div style={{ color: "black" }} className={styles.sectiondata}>
+            {" "}
+            {posts.regions.world.totals.confirmed.toLocaleString()}
+          </div>
+          <div className={styles.sectionnames}>Confirmed</div>
+          <div style={{ color: "#fc5e03" }} className={styles.sectiondata}>
+            {posts.regions.world.totals.critical.toLocaleString()}
+          </div>
+          <div className={styles.sectionnames}>Critical</div>
+          <div style={{ color: "#0ee9cb" }} className={styles.sectiondata}>
+            {" "}
+            {posts.regions.world.totals.recovered.toLocaleString()}
+          </div>
+          <div className={styles.sectionnames}>Recovered</div>
+
+          <div style={{ color: "red" }} className={styles.sectiondata}>
+            {" "}
+            {posts.regions.world.totals.deaths.toLocaleString()}
+          </div>
+          <div className={styles.sectionnames}>Deaths</div>
+
+          <div style={{ color: "#03b1fc" }} className={styles.sectiondata}>
+            {posts.regions.world.totals.vaccinated.toLocaleString()}
+          </div>
+
+          <div className={styles.sectionnames}>Vaccinated</div>
+          <div style={{ marginTop: "30px", fontSize: "15px" }}>
+            {" "}
+            Updated: {moment(posts.last_updated).fromNow(true)} ago
+          </div>
+        </section>
+      </main>
     </>
   );
 }
